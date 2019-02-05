addf035d1a33ae8e95cd89c0e4746c000c843d57
Erick Morales Silva <erick.silva@localhost.localdomain>
2019-02-05 08:49:47 -0600

    v0.0.1 - Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
-import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
+import {
+  AfterViewInit, 
+  Component, 
+  ElementRef, 
+  QueryList, 
+  ViewChild, 
+  ViewChildren
+
+} from '@angular/core';
+
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
+
+import { CourseCardComponent } from './course-card/course-card.component';
+import { HighlightedDirective } from './directives/highlighted.directive';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +23,20 @@ export class AppComponent implements AfterViewInit {
 
     courses = COURSES;
 
+    /**
+     * Read the directive directly:
+     */
+    @ViewChild(HighlightedDirective)
+    highligh: HighlightedDirective
+
+    /**
+     * Or read the directive via its host element.
+     * 
+     * This is very useful when multiple directives are applied
+     * and we want to get directives separately:
+     */
+    @ViewChild(CourseCardComponent, { read: HighlightedDirective })
+    highlighSeparate: HighlightedDirective
 
     @ViewChildren(CourseCardComponent, {read: ElementRef})
     cards : QueryList<ElementRef>;
@@ -23,10 +48,15 @@ export class AppComponent implements AfterViewInit {
 
     ngAfterViewInit() {
 
+      console.log(`HighlightedDirective (read directly) `, this.highligh);
+      console.log(`HighlightedDirective (read via its host element)`, this.highlighSeparate);
     }
 
     onCourseSelected(course:Course) {
 
     }
 
+    onToggleHighlight(isHighlighted: boolean) {
+      console.log(`onToggleHighlight() event value: ${isHighlighted}`);
+    }
 }
---
+++ src/app/directives/highlighted.directive.ts
@@ -0,0 +1,83 @@
+import { Directive, HostBinding, Input, HostListener, Output, EventEmitter } from '@angular/core';
+
+/**
+ * exportAs is used to grab an instance of the directive in the template scope: 
+ */
+
+@Directive({
+  selector: '[highlighted]',
+  exportAs: 'hl'
+})
+export class HighlightedDirective {
+
+  @Input('highlighted')
+  private isHighlighted = false;
+
+  @Output() toggleHighlight = new EventEmitter();
+
+  constructor() { 
+    console.log(`highlighted created !`);
+  }
+
+  /**
+   * @HostBinding is used to apply DOM properties:
+   */
+  @HostBinding('class.highlighted')
+  get cssClasses() {
+    return this.isHighlighted;
+  }
+
+  // @HostBinding('className')
+  // get cssClasses() {
+  //   return 'highlighted';
+  // }
+
+  // @HostBinding('style.border')
+  // get border() {
+  //   return '3px solid blue';
+  // }
+
+  // @HostBinding('style.width')
+  // get width() {
+  //   return '600px';
+  // }
+
+  /**
+   * We can also use @HostBinding to add DOM
+   * attributes:
+   */
+  @HostBinding('attr.disabled')
+  get disabled() {
+    return true;
+  }
+
+  /*************************************/
+
+  /**
+   * @HostListener takes the name of the DOM event as an argument.
+   * @HostListener is great to interact with native DOM events.
+   * 
+   * In case we need we can inject the native event object:
+   */
+  @HostListener('mouseover', ['$event'])
+  mouseOver($event) {
+    console.log($event);
+
+    this.isHighlighted = true;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
+
+  @HostListener('mouseleave')
+  mouseLeave() {
+    this.isHighlighted = false;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
+
+  /**
+   * Directives could have a public API:
+   */
+  public toggle() {
+    this.isHighlighted = !this.isHighlighted;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
+}
